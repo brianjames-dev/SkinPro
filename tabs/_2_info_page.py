8c76c8a952a3d40d852175146ce95701604b8fa0
Brian <brianjames222@yahoo.com>
2025-02-17 23:28:22 -0800

    Updated States to be abbreviations

--- tabs/_2_info_page.py
+++ tabs/_2_info_page.py
@@ -88,16 +88,11 @@ class InfoPage:
 
         ctk.CTkLabel(address_tri_frame, text="State").grid(row=3, column=1, sticky="w", padx=(20, 5))
         self.state_entry = ctk.CTkComboBox(address_tri_frame, border_width=0,
-                                           values=["California", "Alabama", "Alaska", "Arizona", "Arkansas", "Colorado", 
-                                                                      "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", 
-                                                                      "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", 
-                                                                      "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", 
-                                                                      "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", 
-                                                                      "New Hampshire", "New Jersey", "New Mexico", "New York", 
-                                                                      "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", 
-                                                                      "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", 
-                                                                      "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", 
-                                                                      "West Virginia", "Wisconsin", "Wyoming"])
+                                   values=["CA", "AL", "AK", "AZ", "AR", "CO", "CT", "DE", "FL", "GA", "HI", 
+                                           "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", 
+                                           "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", 
+                                           "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", 
+                                           "VT", "VA", "WA", "WV", "WI", "WY"])
         self.state_entry.grid(row=3, column=2, sticky="ew")
 
         ctk.CTkLabel(address_tri_frame, text="Zip").grid(row=3, column=3, sticky="w", padx=(20, 5))
